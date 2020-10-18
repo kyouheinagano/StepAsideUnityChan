c5d865059abebfef8e937b8bd10e4d5d7bec6892
kyouheinagano <72237786+kyouheinagano@users.noreply.github.com>
2020-10-18 17:25:19 +0900

    Initial commit

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour
+{
+    private Camera cam;
+    private bool judgepos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        judgepos = cam.transform.position.z > this.transform.position.z;
+
+        if (judgepos == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
